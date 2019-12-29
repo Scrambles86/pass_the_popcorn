5e679a96e277d57533ee71e81bb8ae3b85603b37
Craig Vidler <craigvidler@Craigs-MacBook-Pro.local>
2019-12-29 14:29:43 +0000

    Added if statment to app.py file

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ if path.exists("env.py"):
   import env 
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
