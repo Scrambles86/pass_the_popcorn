2ce9e85b1f9e64a2bb33479b82c5c70652f50f7f
Craig Vidler <craigvidler@Craigs-MacBook-Pro.local>
2020-01-05 22:28:27 +0000

    Flask errors corrected

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
 from flask import Flask
-from flask_pymongo import PyMongo
 
+APP = Flask(__name__)
 
-@app.route("/")
+print(os.environ.get("MONGO_URI"))
+
+@APP.route("/")
 def hello():
+    """
+    This is the test function
+    """
     return "Hello, Flask!"
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
+    APP.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
