81257aa624e625679d77061afe212ad14c6bfe05
Craig Vidler <craigvidler@Craigs-MacBook-Pro.local>
2020-01-09 22:15:25 +0000

    Added request and url_for from flask to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
