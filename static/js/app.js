a3be861b742ff6eecbeeccd4e2850e3fc02a0c2a
Scrambles86 <craigvidler86@outlook.com>
2020-04-02 16:39:03 +0100

    Function created to open add film form

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,8 @@ function openModal() {
   var modal = document.getElementById("signup");
   modal.style.display = "block"; 
 }
+
+function openModal() {
+  var modal = document.getElementById("addfilm");
+  modal.style.display = "block"; 
+}
