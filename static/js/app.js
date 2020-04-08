0de4b253e903b321cc9290055eb9a4de5aa3f960
Scrambles86 <craigvidler86@outlook.com>
2020-04-08 15:30:53 +0100

    Fixed close button bug

--- static/js/app.js
+++ static/js/app.js
@@ -9,12 +9,6 @@ function openAddModal() {
 }
 
 function closebutton() {
-var closebtns = document.getElementsByClassName("close");
-var i;
-
-/* Loop through the elements, and hide the parent, when clicked on */
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-  this.parentElement.style.display = 'none';
-});
+  var closebtn = document.getElementsByClassName("close");
+  closebtn.style.display = "none"; 
 }
