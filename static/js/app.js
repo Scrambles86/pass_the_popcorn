77bff4efde7d9652202a6bbf6080242c85706d0d
Scrambles86 <craigvidler86@outlook.com>
2020-04-08 18:21:32 +0100

    Function created to ensure working close button on all modals

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,35 @@
+var signup = document.getElementById("signup");
+var addfilm = document.getElementById("addfilm");
+var editfilm = document.getElementById("editfilm");
+var deletefilm = document.getElementById("deletefilm");
+
 function openSignup() {
-  var modal = document.getElementById("signup");
-  modal.style.display = "block"; 
+  signup.style.display = "block"; 
 }
 
 function openAddModal() {
-  var filmform = document.getElementById("addfilm");
-  filmform.style.display = "block"; 
+  addfilm.style.display = "block"; 
+}
+
+function openEditModal() {
+  editfilm.style.display = "block"; 
+}
+
+function openDeleteModal() {
+  deletefilm.style.display = "block"; 
 }
 
 function closeModal() {
-  var closebtn = document.getElementById("signup");
-  closebtn.style.display = "none"; 
+  if(signup.style.display != "none";) {
+    signup.style.display = "none";
+  }
+  if(addfilm.style.display != "none";) {
+    addfilm.style.display = "none";
+  }
+  if(editfilm.style.display != "none";) {
+    editfilm.style.display = "none";
+  }
+  if(deletefilm.style.display != "none";) {
+    deletefilm.style.display = "none";
+  }
 }
