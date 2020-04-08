d2bfa0c7435f686a2f5066195d298bd83cfc0380
Scrambles86 <craigvidler86@outlook.com>
2020-04-08 18:30:35 +0100

    Or rule added to functions

--- static/js/app.js
+++ static/js/app.js
@@ -20,16 +20,16 @@ function openDeleteModal() {
 }
 
 function closeModal() {
-  if(signup.style.display != "none") {
+  if(signup.style.display != "none" || signup.style.display == null) {
     signup.style.display = "none";
   }
-  if(addfilm.style.display != "none") {
+  if(addfilm.style.display != "none" || addfilm.style.display == null) {
     addfilm.style.display = "none";
   }
-  if(editfilm.style.display != "none") {
+  if(editfilm.style.display != "none" || editfilm.style.display == null) {
     editfilm.style.display = "none";
   }
-  if(deletefilm.style.display != "none") {
+  if(deletefilm.style.display != "none" || deletefilm.style.display == null) {
     deletefilm.style.display = "none";
   }
 }
