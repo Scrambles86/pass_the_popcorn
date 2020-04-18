dd66312df8b8ba42f5a09895feb5b75eabb91d4e
Scrambles86 <craigvidler86@outlook.com>
2020-04-18 18:43:18 +0100

    Function created to load seperate modal content within one core modal

--- static/js/app.js
+++ static/js/app.js
@@ -3,40 +3,49 @@ fetch("http://www.omdbapi.com/?apikey=ac155d96&")
   .then (data => console.log(data))
 
 
-let signup = document.getElementById("signup");
-let addfilm = document.getElementById("addfilm");
-let editfilm = document.getElementById("editfilm");
-let deletefilm = document.getElementById("deletefilm");
-
+// let signup = document.getElementById("signup");
+// let addfilm = document.getElementById("addfilm");
+// let editfilm = document.getElementById("editfilm");
+// let deletefilm = document.getElementById("deletefilm");
+let formstyle = document.getElementsByClassName("formstyle");
+let contentone = document.getElementById("contentone");
+let contenttwo = document.getElementById("contenttwo");
+let contentthree = document.getElementById("contentthree");
+let contentfour = document.getElementById("contentfour");
 
 function openSignup() {
-  signup.style.display = "block"; 
+  formstyle.style.display = "block";
+  contentone.style.display = "block";
 }
 
 function openAddModal() {
-  addfilm.style.display = "block"; 
+  formstyle.style.display = "block"; 
+  contenttwo.style.display = "block";
 }
 
 function openEditModal() {
-  editfilm.style.display = "block"; 
+  formstyle.style.display = "block"; 
+  contentthree.style.display = "block";
 }
 
 function openDeleteModal() {
-  deletefilm.style.display = "block"; 
+  formstyle.style.display = "block"; 
+  contentfour.style.display = "block";
 }
 
 function closeModal() {
   // if element is present on page and its display value isn't "none"
-  if (signup && signup.style.display != "none") {
-    signup.style.display = "none";
-  }
-  if (addfilm && addfilm.style.display != "none") {
-    addfilm.style.display = "none";
-  }
-  if (editfilm && editfilm.style.display != "none") {
-    editfilm.style.display = "none";
-  }
-  if (deletefilm && deletefilm.style.display != "none") {
-    deletefilm.style.display = "none";
-  }
+  // if (signup && signup.style.display != "none") {
+  //   signup.style.display = "none";
+  // }
+  // if (addfilm && addfilm.style.display != "none") {
+  //   addfilm.style.display = "none";
+  // }
+  // if (editfilm && editfilm.style.display != "none") {
+  //   editfilm.style.display = "none";
+  // }
+  // if (deletefilm && deletefilm.style.display != "none") {
+  //   deletefilm.style.display = "none";
+  // }
+  formstyle.style.display = "none";
 }
