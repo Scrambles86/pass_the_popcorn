2a08d62f11f8ff626e07ebf1b962dd2bf45ab34e
Scrambles86 <craigvidler86@outlook.com>
2020-05-10 17:38:17 +0100

    Navbar updated

--- app.py
+++ app.py
@@ -4,58 +4,52 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 
-hash = generate_password_hash('foobar')
-check_password_hash(hash, 'foobar')
-
-
+# Created instance of Flask
 APP = Flask(__name__)
 
+# MongoDB settings - URI and Database name. Settings hidden in env file
 APP.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
 APP.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
-
+#Pymongo instance created
 MONGO = PyMongo(APP)
 
+@APP.route('/index')
+def index():
+    return render_template('pages/index.html')
+
+
+# @APP.route('/<password>')
+# def login_page(password):
+
+#     hashed_value = generate_password_hash(password)
+
+#     stored_password = 'pbkdf2:sha256:150000$NawC8dmS$996a3db0462a554d1e3090d4d216c80642726bb65e3eb9869341caf988e3bc5d'
+
+#     result = check_password_hash(stored_password, password)
+
+#     return render_template('pages/loginpage.html')
+
 # @APP.route('/')
-# def index():
-#     return render_template('pages/index.html')
+# def user_login():
+#     if 'username' in session:
+#         return 'You are logged in as' + session['username']    
+#     return render_template('pages/userpage.html')
 
-@APP.route('/login_page')
-def login_page():
-    return render_template('pages/loginpage.html')
+# @APP.route('/login')
+# def login():
+#     return ''
 
-@APP.route('/<password>')
-def index(password):
+# @APP.route('/register', methods=['POST', 'GET'])
+# def register():
+#     if request.method == 'POST' :
+#         users = MONGO.db.users
+#         existing_user = users.find_one({'name' : request.form['username']})
 
-    hashed_value = generate_password_hash(password)
+#         if existing_user is None:
+#             hash = generate_password_hash(request.form['password'])
 
-    stored_password = 'pbkdf2:sha256:150000$NawC8dmS$996a3db0462a554d1e3090d4d216c80642726bb65e3eb9869341caf988e3bc5d'
-
-    result = check_password_hash(stored_password, password)
-
-    return str(result)
-
-@APP.route('/')
-def user_login():
-    if 'username' in session:
-        return 'You are logged in as' + session['username']
-    
-    return render_template('pages/userpage.html')
-
-@APP.route('/login')
-def login():
-    return ''
-
-@APP.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST' :
-        users = MONGO.db.users
-        existing_user = users.find_one({'name' : request.form['username']})
-
-        if existing_user is None:
-            hash = generate_password_hash(request.form['password'])
-
-    return ''
+#     return ''
 
 
 
@@ -78,9 +72,10 @@ def add_review(posts):
 @APP.route("/userpage")
 def archives():
     """
-    Renders template for archive page
+    Returns users to their personal collection page
     """
-    return render_template("pages/userpage.html")
+    loggedIn = True if 'user' in session else False
+    return render_template("pages/userpage.html", films=MONGO.db.movie_data.find())
 
 
 
